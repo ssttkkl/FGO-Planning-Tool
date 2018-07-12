66def576b2dc0d0b67e51eca7e742aadaec264cf
ssttkkl <huang.wen.long@hotmail.com>
2018-07-12 19:27:15 +0800

    remove files from master branch

--- app/src/main/java/com/ssttkkl/fgoplanningtool/Dispatchers.kt
+++
@@ -1,8 +0,0 @@
-package com.ssttkkl.fgoplanningtool
-
-import kotlinx.coroutines.experimental.newSingleThreadContext
-
-object Dispatchers {
-    val db = newSingleThreadContext("db")
-    val file = newSingleThreadContext("file")
-}
--- app/src/main/java/com/ssttkkl/fgoplanningtool/ui/costitemlist/CostItemListEntity.kt
+++
@@ -1,7 +0,0 @@
-package com.ssttkkl.fgoplanningtool.ui.costitemlist
-
-data class CostItemListEntity(val name: String,
-                              val type: String,
-                              val need: Int,
-                              val own: Int,
-                              val imgUri: String)
