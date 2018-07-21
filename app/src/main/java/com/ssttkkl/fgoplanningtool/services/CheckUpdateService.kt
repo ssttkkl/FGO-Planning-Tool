b0cd1547e189ed20f19b8bd1a2fabd7276bd8445
ssttkkl <huang.wen.long@hotmail.com>
2018-07-21 12:23:53 +0800

    修正Oreo不能接受通知的问题（原来NotificationChannel.DEFAULT_CHANNEL_ID是不给用的啊）

--- app/src/main/java/com/ssttkkl/fgoplanningtool/services/CheckUpdateService.kt
+++ app/src/main/java/com/ssttkkl/fgoplanningtool/services/CheckUpdateService.kt
@@ -1,9 +1,13 @@
 package com.ssttkkl.fgoplanningtool.services
 
-import android.app.*
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.IBinder
 import android.support.v4.app.NotificationCompat
 import android.util.Log
@@ -21,7 +25,6 @@ import org.apache.commons.io.FileUtils
 import java.io.File
 import java.net.URL
 import java.util.*
-import javax.net.ssl.HttpsURLConnection
 
 class CheckUpdateService : Service() {
     override fun onBind(intent: Intent): IBinder? = null
@@ -53,37 +56,53 @@ class CheckUpdateService : Service() {
     }
 
     private fun notifyResPackUpdate(latestInfo: ResPackLatestInfo) {
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID_UPDATE, getString(R.string.channel_updateNotification_checkupdare), NotificationManager.IMPORTANCE_HIGH)
+            manager.createNotificationChannel(channel)
+        }
+
         val intent = Intent(this, PreferencesActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        val notification = NotificationCompat.Builder(this, NotificationChannel.DEFAULT_CHANNEL_ID)
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID_UPDATE)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(getString(R.string.resPackUpdateAvailable_checkupdate))
                 .setContentText("${latestInfo.content}(${latestInfo.releaseDate})")
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .build()
-
-        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         manager.notify(0, notification)
     }
 
     private fun notifyAppUpdate(latestInfo: AppLatestInfo) {
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID_UPDATE, getString(R.string.channel_updateNotification_checkupdare), NotificationManager.IMPORTANCE_HIGH)
+            manager.createNotificationChannel(channel)
+        }
+
         val intent = Intent().apply {
             action = Intent.ACTION_VIEW
             data = Uri.parse(latestInfo.downloadLink)
         }
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        val notification = NotificationCompat.Builder(this, NotificationChannel.DEFAULT_CHANNEL_ID)
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID_UPDATE)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(getString(R.string.appUpdateAvailable_checkupdate))
                 .setContentText("${latestInfo.versionName}(${latestInfo.versionCode})")
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .build()
-
-        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         manager.notify(0, notification)
+    }
+
+    companion object {
+        private const val CHANNEL_ID_UPDATE = "update"
     }
 }
