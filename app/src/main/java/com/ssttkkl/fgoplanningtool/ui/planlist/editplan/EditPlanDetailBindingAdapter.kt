6ae1432134d2e09f5d2a0232ba64f9f8de2cdf06
ssttkkl <huang.wen.long@hotmail.com>
2019-01-19 11:08:18 +0800

    修正筛选存在的bug

--- app/src/main/java/com/ssttkkl/fgoplanningtool/ui/planlist/editplan/EditPlanDetailBindingAdapter.kt
+++ app/src/main/java/com/ssttkkl/fgoplanningtool/ui/planlist/editplan/EditPlanDetailBindingAdapter.kt
@@ -7,13 +7,17 @@ import androidx.databinding.InverseBindingAdapter
 object EditPlanDetailBindingAdapter {
     @BindingAdapter("selection")
     @JvmStatic
-    fun setSelection(spinner: Spinner, selection: ObservablePlan.SkillLevel) {
-        spinner.setSelection(selection.ordinal)
+    fun setSelection(spinner: Spinner, selection: ObservablePlan.SkillLevel?) {
+        spinner.setSelection(selection?.ordinal ?: -1)
     }
 
     @InverseBindingAdapter(attribute = "selection", event = "onItemSelected")
     @JvmStatic
-    fun getSelection(spinner: Spinner): ObservablePlan.SkillLevel {
-        return ObservablePlan.SkillLevel.values()[spinner.selectedItemPosition]
+    fun getSelection(spinner: Spinner): ObservablePlan.SkillLevel? {
+        return try {
+            ObservablePlan.SkillLevel.values()[spinner.selectedItemPosition]
+        } catch (_: Exception) {
+            null
+        }
     }
 }
